6011235c07f3b5352cff9fc9c7cd3c1fe20c4a19
Gerard Balaoro <gerardb@humanforce.com>
2026-08-11 12:12:11 +0800

    feat(forge): add tests and improve debug script

--- plugins/forge/scripts/debug.ts
+++ plugins/forge/scripts/debug.ts
@@ -3,28 +3,38 @@ import { handshake } from "../lib/handshake";
 const forge = await handshake();
 
 console.group("Handshake");
-console.log(`  → Path: ${forge.path}`);
-console.log(`  → URI: ${forge.uri}`);
-console.log(`  → Token: ${forge.token}`);
+console.log(`→ Path: ${forge.path}`);
+console.log(`→ URI: ${forge.uri}`);
+console.log(`→ Token: ${forge.token}`);
 console.groupEnd();
 
 const agents = await forge.agents();
 
 console.group("Agents");
 for (const [name, agent] of Object.entries(agents)) {
-  console.log(`  → ${name}: ${agent?.model}`);
+  console.log(`→ ${name}: ${agent?.model} / ${agent?.variant || "default"}`);
 }
 console.groupEnd();
 
 const provider = await forge.provider();
 
 console.group("Provider");
-console.log(`  → Name: ${provider?.name}`);
-console.log(`  → URI: ${provider?.api}`);
-console.log(`  → Models:`);
-console.group();
-for (const model of Object.keys(provider?.models ?? {})) {
-  console.log(`    → ${model}`);
-}
+console.log(`→ Name: ${provider?.name}`);
+console.log(`→ URI: ${provider?.api}`);
 console.groupEnd();
+
+console.group("Models");
+for (const [id, model] of Object.entries(provider?.models ?? {})) {
+  console.group(`${model.name}`);
+  console.log(`→ ID: ${model.id || id}`);
+  console.log(`→ Context: ${model.limit?.context}`);
+  if (Array.isArray(model.modalities?.input)) {
+    console.log(`→ Input: ${model.modalities.input.join(", ")}`);
+  }
+  if (Array.isArray(model.modalities?.output)) {
+    console.log(`→ Output: ${model.modalities.output.join(", ")}`);
+  }
+  console.groupEnd();
+}
+
 console.groupEnd();
